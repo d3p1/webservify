cca1aca4120320eac89f0f4c9368de7f99dfab19
C. M. de Picciotto <d3p1@d3p1.dev>
2025-02-18 19:50:35 +0000

    feat: improve execute remote command logic [#1]

--- src/bin/lib/utils/execute-remote-command.sh
+++ src/bin/lib/utils/execute-remote-command.sh
@@ -6,13 +6,37 @@
 ##
 
 ##
-# Execute script
+# Execute remote command
 #
 # @param  string $1 Remote user
 # @param  string $2 Remote host
-# @param  string $3 Command
+# @param  string $3 Remote port
+# @param  string $4 Command
 # @return void
+# @note   If `ssh` credentials are not specified,
+#         it will be used
+#         `REMOTE_SSH_USER`, `REMOTE_SSH_HOST` and `REMOTE_SSH_PORT`
+#         environment variables
 ##
 execute_remote_command() {
-    ssh "$1:$2" $3
+    local ssh_user
+    local ssh_host
+    local ssh_port
+
+    ssh_user="$1"
+    if [ -z "$ssh_user" ]; then
+        ssh_user="$REMOTE_SSH_USER"
+    fi
+
+    ssh_host="$2"
+    if [ -z "$ssh_host" ]; then
+        ssh_host="$REMOTE_SSH_HOST"
+    fi
+
+    ssh_port="$3"
+    if [ -z "$ssh_port" ]; then
+        ssh_port="$REMOTE_SSH_PORT"
+    fi
+
+    ssh -p "$ssh_port" "$ssh_user:$ssh_host" $4
 }
